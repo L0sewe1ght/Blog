40f89a0257d4d19800a264334cd33823114df8a4
Chr0mium24 <3294212389@qq.com>
2025-08-26 02:37:50 +0800

    external editor

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "编辑",
-			url: "editor", // Internal links should not include the base path, as it is automatically added
-			external: false, // Show an external link icon and will open in a new tab
+			url: "https://zhengteng39jy-ship-it.github.io/Blog/editor/", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
