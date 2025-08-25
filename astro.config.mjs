405228ddda7b90c322d11f7a882d37c5deb01a09
Chr0mium24 <3294212389@qq.com>
2025-08-26 02:23:53 +0800

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://zhengteng39jy-ship-it.github.io/Blog/",
-	base: "/",
+	base: "/Blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
